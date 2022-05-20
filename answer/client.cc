ee4999abf3b27a0416f8d46123f71b3f5f5ae874
hengyi1997 <zhaohy19@mails.tsinghua.edu.cn>
2022-05-21 02:17:47 +0800

    first version

--- answer/client.cc
+++ answer/client.cc
@@ -1,6 +1,5 @@
 #include <chrono>
-#include <iostream>
-#include <random>
+#include <cmath>
 #include <string>
 #include <thread>
 
@@ -14,22 +13,48 @@ class Client {
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
-  void send_numbers_regularly(double a, double b) {
+  void send_numbers_regularly() {
     while (true) {
-      auto send_pair = generate_random_pair();
+      auto send_pair = generate_suitable_pair();
       send_numbers_once(send_pair.first, send_pair.second);
       std::this_thread::sleep_for(interval_);
     }
   }
 #pragma clang diagnostic pop
 
-  static std::pair<double, double> generate_random_pair() {
-    double a = (rand() % 10 + 1) / 10.0;
-    double b = (rand() % 10 + 1) / 10.0;
-    return std::make_pair(a, b);
+  std::pair<double, double> generate_suitable_pair() {
+    std::pair<double, double> uniform = generate_uniform_random_pair();
+    std::pair<double, double> normal_independent = generate_std_normal_random_pair(uniform);
+    std::pair<double, double> suitable_pair = generate_random_pair_sigma(normal_independent);
+    return suitable_pair;
   }
 
  private:
+  std::pair<double, double> generate_random_pair_sigma(std::pair<double, double> normal) const {
+    double normal_a = normal.first;
+    double normal_b = normal.second;
+    double theta = 0.5 * std::asin(sigma_);
+    double normal_sigma_a = std::cos(theta) * normal_a + std::sin(theta) * normal_b;
+    double normal_sigma_b = std::cos(theta) * normal_b + std::sin(theta) * normal_a;
+    return std::make_pair(normal_sigma_a, normal_sigma_b);
+  }
+
+  static std::pair<double, double> generate_uniform_random_pair() {
+    double a = (rand() % 10001) / 10000.0;
+    double b = (rand() % 10001) / 10000.0;
+    return std::make_pair(a, b);
+  }
+
+  // Use Box-Mullelr transform
+  static std::pair<double, double> generate_std_normal_random_pair(std::pair<double, double> uniform) {
+    double a = uniform.first;
+    double b = uniform.second;
+    const double pi = 3.1415926535;
+    double normal_a = std::sqrt((-2) * std::log(a)) * std::cos(2 * pi * b);
+    double normal_b = std::sqrt((-2) * std::log(a)) * std::sin(2 * pi * b);
+    return std::make_pair(normal_a, normal_b);
+  }
+
   void send_numbers_once(double a, double b) {
     std::string body = "[" + std::to_string(a) + "A" + std::to_string(b) + "]";
     cli_.Post("/data", body, "text/plain");
@@ -42,9 +67,6 @@ class Client {
 };
 
 int main() {
-  Client client("localhost", "20000", std::chrono::milliseconds{1000}, 0.1);
-  client.send_numbers_regularly(1.234, 5.678);
-  //  httplib::Client cli1("http://127.0.0.1:20000");
-  //  auto rest = cli1.Post("/data", "123123123", "text/plain");
-  //  std::cout << rest->body << std::endl;
+  Client client("localhost", "20000", std::chrono::milliseconds{500}, 0.1);
+  client.send_numbers_regularly();
 }
--- answer/server.cc
+++ answer/server.cc
@@ -16,11 +16,17 @@ class Server {
  public:
   Server() = default;
   Server(std::string host, int port, std::chrono::seconds W, std::chrono::seconds S)
-      : statistic_region_W_(W), statistic_interval_S_(S), host_(std::move(host)), port_(port) {}
+      : statistic_region_W_(W), statistic_interval_S_(S), host_(std::move(host)), port_(port), next_compute_time_(0) {}
 
-  void start_server_and_receive_nums() {
+  void start_server(std::chrono::milliseconds start_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+                        std::chrono::system_clock::now().time_since_epoch())) {
+    next_compute_time_ = start_time.count() + statistic_interval_S_.count();
     svr_.Post("/data", [this](const httplib::Request& req, httplib::Response& res) {
       auto recv = req.body;
+      auto recv_time =
+          std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+      // save date
       for (auto i = recv.cbegin() + 1; i != recv.cend() - 1; ++i) {
         if (std::isupper(*i)) {
           double a = std::stod(std::string(recv.cbegin() + 1, i));
@@ -28,13 +34,112 @@ class Server {
           received_data_.push_back({a, b,
                                     std::chrono::duration_cast<std::chrono::milliseconds>(
                                         std::chrono::system_clock::now().time_since_epoch())});
-          std::cout << received_data_.back().a << "  " << received_data_.back().b << "   "
-                    << received_data_.back().timestamp.count() << std::endl;
+          //          std::cout << received_data_.back().a << "  " << received_data_.back().b << "   "
+          //                    << received_data_.back().timestamp.count() << std::endl;
         }
+      }
+
+      // compute or not
+      if (recv_time.count() >= next_compute_time_) {
+        std::cout << "vvvvvvvvvv" << std::endl;
+        compute_statistics_once();
+        next_compute_time_ += statistic_interval_S_.count();
+        std::cout << "^^^^^^^^^^" << std::endl;
       }
     });
 
     svr_.listen(host_.c_str(), port_);
+  }
+
+  void compute_statistics_once() {
+    auto vec_pair = separate_num_pairs_during_region();
+    compute_cov_cor_and_output(vec_pair.first, vec_pair.second);
+  }
+
+ private:
+  static void compute_cov_cor_and_output(std::vector<double>& vec_a, std::vector<double>& vec_b) {
+    auto vec_a_times_b = veca_times_vecb(vec_a, vec_b);
+    auto vec_a_info = compute_basic_statistic_variables(vec_a, "ai");
+    auto vec_b_info = compute_basic_statistic_variables(vec_b, "bi");
+    auto vec_ab_info = compute_basic_statistic_variables(vec_a_times_b, "ai * bi");
+    auto cov_ab = vec_ab_info.first - vec_a_info.first * vec_b_info.first;
+    auto cor_ab = cov_ab / (vec_a_info.second * vec_b_info.second);
+    std::cout << "Cov(ai, bi): " << cov_ab << " Cor(ai, bi): " << cor_ab << std::endl;
+  }
+
+  std::pair<std::vector<double>, std::vector<double>> separate_num_pairs_during_region() {
+    std::chrono::milliseconds region = std::chrono::duration_cast<std::chrono::milliseconds>(statistic_region_W_);
+    auto now =
+        std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+    auto start_time = now - region;
+    std::vector<double> ai_vec{}, bi_vec{};
+    for (auto itr = received_data_.cbegin(); itr != received_data_.end(); ++itr) {
+      if (itr->timestamp < start_time) continue;
+      if (itr->timestamp > now) break;
+      ai_vec.push_back(itr->a);
+      bi_vec.push_back(itr->b);
+    }
+    return std::make_pair(ai_vec, bi_vec);
+  }
+
+  static double compute_average(const std::vector<double>& vec) {
+    double sum{};
+    for (auto i : vec) {
+      sum += i;
+    }
+    return sum / static_cast<double>(vec.size());
+  }
+
+  static std::pair<double, double> compute_basic_statistic_variables(std::vector<double>& vec,
+                                                                     const std::string& title = "") {
+    std::vector<double> res_vec = simple_bubble_sort(vec);
+
+    double max_value = *(res_vec.cend() - 1);
+    double min_value = *res_vec.cbegin();
+
+    double average = compute_average(vec);
+
+    double median = ((vec.size() % 2) == 0) ? ((vec.at(vec.size() / 2) + vec.at(vec.size() / 2 - 1)) / 2)
+                                            : (vec.at(vec.size() / 2));
+
+    double variance{};
+    for (auto i : vec) {
+      variance += (i - average) * (i - average);
+    }
+    variance /= static_cast<double>(vec.size());
+    double std_deviation = std::sqrt(variance);
+
+    std::cout << title << " :: "
+              << " max: " << max_value << " min: " << min_value << " average: " << average
+              << " standard deviation: " << std_deviation << " median: " << median << std::endl;
+
+    return std::make_pair(average, std_deviation);
+  }
+
+  static std::vector<double> veca_times_vecb(const std::vector<double>& vec_a, const std::vector<double>& vec_b) {
+    std::vector<double> times{};
+    for (size_type i = 0; i < vec_a.size(); ++i) {
+      times.push_back(vec_a.at(i) * vec_b.at(i));
+    }
+    return times;
+  }
+
+  using size_type = std::vector<double>::size_type;
+  static std::vector<double>& simple_bubble_sort(std::vector<double>& vec) {
+    if (vec.empty() || vec.size() < 2) return vec;
+
+    for (size_type i = vec.size() - 1; i > 0; --i) {
+      for (size_type j = 0; j < i; ++j) {
+        if (vec.at(j) > vec.at(j + 1)) swap(vec, j, j + 1);
+      }
+    }
+    return vec;
+  }
+
+  static void swap(std::vector<double>& vec, size_type i, size_type j) {
+    double temp = vec.at(i);
+    vec.at(i) = vec.at(j);
+    vec.at(j) = temp;
   }
 
  private:
@@ -42,19 +147,12 @@ class Server {
   std::chrono::seconds statistic_interval_S_{};
   std::string host_{};
   int port_{};
+  int64_t next_compute_time_{};
   httplib::Server svr_{};
 
   std::vector<received_data> received_data_{};
 };
 int main() {
-  //  httplib::Server svr;
-  //  svr.Post("/data", [](const httplib::Request& req, httplib::Response& res) {
-  //    auto recv = req.body;
-  //    std::cout << "3333" + recv + "4444" << std::endl;
-  //    res.set_content("33333" + recv + "4444", "text/plain");
-  //  });
-  //  svr.listen("127.0.0.1", 20000);
-
-  Server server("localhost", 20000, std::chrono::seconds{3}, std::chrono::seconds{3});
-  server.start_server_and_receive_nums();
+  Server server("localhost", 20000, std::chrono::seconds{5}, std::chrono::seconds{3});
+  server.start_server();
 }
